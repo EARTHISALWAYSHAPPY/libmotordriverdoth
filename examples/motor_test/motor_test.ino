bea2b53c71da6c32e2678a458d73b3bf32b42ede
Knnthiti <120441472+Knnthiti@users.noreply.github.com>
2024-07-08 19:45:52 +0700

    Update motor_test.ino new update (consturctor)

--- examples/motor_test/motor_test.ino
+++ examples/motor_test/motor_test.ino
@@ -1,8 +1,12 @@
-#include <libmotordriver.h>
-//Creating class objects LibMotorDriver can call methods.
-LibMotorDriver motor_ctrl;
+#include "libmotordriver.h"
+#define L 9
+#define INL1 2
+#define INL2 3
+#define INR1 4
+#define INR2 5
+#define R 10
+LibMotorDriver motor_ctrl(L, INL1, INL2, INR1, INR2, R);
 void setup() {
-
 
 }
 void loop() {
@@ -17,3 +21,5 @@ void loop() {
   motor_ctrl.motor_stop_all();
   delay(1000);
 }
+
+
